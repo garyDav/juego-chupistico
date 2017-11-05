3388d9693f29586f5394ea33a3151496fb8e6c64
garyDav <gary.2810.dav@gmail.com>
2017-11-05 00:35:37 -0400

    Aleatorio

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,4 @@
 // Retorna un número aleatorio entre min (incluido) y max (excluido)
-function getRandomArbitrary(min, max) {
+function random(min, max) {
   return Math.floor((Math.random() * (max - min) + min));
-}
-
-function enviarDatos() {
-	getRandomArbitrary(2,7);
 }
